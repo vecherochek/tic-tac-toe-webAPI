5213324ffcaa68818127e218656b81fdf1bf7074
Polina Bureeva <pbureeva@gmail.com>
2023-03-11 03:28:07 +0300

    figuretype enum

---
+++ TicTacToe.API/BLL/Enums/FigureType.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe.API.BLL.Enums;
+
+/// <summary>
+/// 
+/// </summary>
+public enum FigureType
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    None = ' ',
+
+    /// <summary>
+    /// 
+    /// </summary>
+    Cross = 'X',
+
+    /// <summary>
+    /// 
+    /// </summary>
+    Zero = '0'
+}

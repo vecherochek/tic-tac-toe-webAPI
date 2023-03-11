fc66931da465f6a30677474010a9ac88f9e50371
Polina Bureeva <pbureeva@gmail.com>
2023-03-11 03:30:26 +0300

    created bdcontexts

---
+++ TicTacToe.API/DAL/Repositories/Contexts/GamesDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicTacToe.API.BLL.Models;
+
+namespace TicTacToe.API.DAL.Repositories.Contexts;
+
+public class GamesDbContext : DbContext
+{
+    public DbSet<Game> Games { get; set; }
+
+    public GamesDbContext(DbContextOptions<GamesDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=tic-tac-toe-db;Username=postgres;Password=qwerty");
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Game>(options =>
+        {
+            options.HasKey(x => x.Id);
+            options.OwnsOne(game => game.Steps, ownedNavigationBuilder => ownedNavigationBuilder.ToJson());
+        });
+    }
+}

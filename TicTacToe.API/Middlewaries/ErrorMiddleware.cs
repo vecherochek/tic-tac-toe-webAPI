072de3b745f2f22e4bbc9ee1c4b8d9a8fba96908
Polina Bureeva <pbureeva@gmail.com>
2023-03-11 03:37:29 +0300

    created custom error middleware

---
+++ TicTacToe.API/Middlewaries/ErrorMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace TicTacToe.API.Middlewaries;
+
+internal sealed class ErrorMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        try
+        {
+            await _next.Invoke(context);
+        }
+        catch (ValidationException e)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsync(new
+            {
+                Message = e.Message
+            }.ToString());
+        }
+        catch (Exception)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        }
+    }
+}

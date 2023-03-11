31d553f19f58a7e716a6145d453b4af08ec9e90c
Polina Bureeva <pbureeva@gmail.com>
2023-03-11 03:29:42 +0300

    extensions for lislStep

---
+++ TicTacToe.API/BLL/Extensions/ListStepsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using TicTacToe.API.BLL.Enums;
+using TicTacToe.API.BLL.Models;
+
+namespace TicTacToe.API.BLL.Extensions;
+
+public static class ListStepsExtensions
+{
+    internal static char[][] GetGameMatrix(this List<Step> steps)
+    {
+        var matrix = new char[3][];
+        for (int i = 0; i < 3; i++)
+            matrix[i] = new char[3];
+
+        foreach (var step in steps)
+        {
+            var cell = step.Cell;
+            matrix[cell.X][cell.Y] = step.Type == FigureType.Cross ? 'X' : 'O';
+        }
+
+        return matrix;
+        // this should be in redis too
+    }
+}

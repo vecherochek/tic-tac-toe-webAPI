202e17506a38b0695fba72d71a1a7097bda7016f
Polina Bureeva <pbureeva@gmail.com>
2023-03-11 23:36:18 +0300

    added migrations

---
+++ TicTacToe.API/Migrations/20230311183426_InitialCreate.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using TicTacToe.API.BLL.Models;
+
+#nullable disable
+
+namespace TicTacToe.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nickname = table.Column<string>(type: "text", nullable: false),
+                    SaltedPassword = table.Column<byte[]>(type: "bytea", nullable: false),
+                    IsOnline = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerXId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PlayerOId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Steps = table.Column<List<Step>>(type: "jsonb", nullable: true),
+                    WinnerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    IsFinished = table.Column<bool>(type: "boolean", nullable: false),
+                    RoomId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_RoomId",
+                table: "Games",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_Nickname",
+                table: "Players",
+                column: "Nickname",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}

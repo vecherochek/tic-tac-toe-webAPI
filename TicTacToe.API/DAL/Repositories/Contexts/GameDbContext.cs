56e39244ea998b85df3f19e727c6a06fb4bd9485
Polina Bureeva <pbureeva@gmail.com>
2023-03-12 16:33:02 +0300

    fixed connection string

--- TicTacToe.API/DAL/Repositories/Contexts/GameDbContext.cs
+++ TicTacToe.API/DAL/Repositories/Contexts/GameDbContext.cs
@@ -8,7 +8,7 @@ public class GameDbContext : DbContext
     public DbSet<Game> Games { get; set; }
     public DbSet<Player> Players { get; set; }
     public DbSet<Room> Rooms { get; set; }
-    
+
     public GameDbContext(DbContextOptions<GameDbContext> options)
         : base(options)
     {
@@ -17,7 +17,7 @@ public class GameDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=qwerty");
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=gamedb;Username=postgres;Password=qwerty");
     }
 
     protected override void OnModelCreating(ModelBuilder builder)
@@ -27,13 +27,13 @@ public class GameDbContext : DbContext
             options.HasKey(x => x.Id);
             //options.OwnsOne(game => game.Steps, ownedNavigationBuilder => ownedNavigationBuilder.ToJson());
         });
-        
+
         builder.Entity<Player>(entity =>
         {
             entity.HasKey(x => x.Id);
             entity.HasIndex(e => e.Nickname).IsUnique();
         });
-        
+
         builder.Entity<Room>().HasKey(x => x.Id);
     }
 }
